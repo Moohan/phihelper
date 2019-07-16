39ca3af7f531e62305ec4e8349a8ee0e4c72f43f
James McMahon <jamesmcmahon0@gmail.com>
2019-07-16 17:29:40 +0100

    adding aditional tests

--- tests/testthat/test-valid_chi.R
+++ tests/testthat/test-valid_chi.R
@@ -1,7 +1,21 @@
 test_that("valid chi identifies bad chi numbers", {
+  # Correct CHI
+  expect_true(valid_chi("1504915593"))
+
+  # Good dates, bad check digit
+  expect_false(valid_chi("1504915594"))
+
   # Correct check-digit, obviously incorrect date
   expect_false(valid_chi("9999999999"))
 
   # Incorrect length
   expect_false(valid_chi("123"))
+
+  # Works in a mutate
+  test_tib <- tibble::tibble(chi = c("1504915593", "1504915594", "9999999999", "123"),
+                             known_valid = c(TRUE, FALSE, FALSE, FALSE))
+
+  test_tib <- dplyr::mutate(test_tib, check_valid = valid_chi(chi))
+
+  expect_equal(test_tib$known_valid, test_tib$check_valid)
 })
