eb4e397998df2aaa96ff1c9396cb685f8bdd73bb
James McMahon <jamesmcmahon0@gmail.com>
2019-07-16 17:27:03 +0100

    improve tests and documentation for round

--- R/round.R
+++ R/round.R
@@ -1,11 +1,15 @@
 #' Function to mask the base round with janitor's round half up
 #'
-#' @param ...
+#' @param x number to round
+#' @param digits how many digits should be displayed after the decimal point?
 #'
-#' @return
+#' @return x rounded
 #' @export
 #'
 #' @examples
-round <- function(...) {
-  janitor::round_half_up(...)
+#' round(0.5)
+#' round(1.135, 2)
+round <- function(x, digits = 0) {
+  janitor::round_half_up(x, digits)
 }
+
