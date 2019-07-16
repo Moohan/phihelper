67e1f5f25f63eda19bd651c3b005c40052a29ec5
James McMahon <jamesmcmahon0@gmail.com>
2019-07-16 17:28:31 +0100

    improving format postcode documentaion

--- R/format_postcode.R
+++ R/format_postcode.R
@@ -1,22 +1,22 @@
 #' Format postcodes into a consistent format
 #'
-#' @param postcode_var
-#' @param length (Optional, default: 7-char)
+#' @param postcode the postcode to format
+#' @param length Optional, default: 7-char
 #'
 #' @return postcode
 #' @export
 #'
 #' @examples
 #' data %<>% mutate(postcode = format_postcode(postcode))
-format_postcode <- function(postcode_var, length = 7) {
+format_postcode <- function(postcode, length = 7) {
   # Extract the Outward code, this will be 2 x (letters or numbers)
   # Possibly followed by a number and then a letter
   # e.g G1, EH3 etc...
   outward_code <-
-    stringr::str_extract(postcode_var, "^([[:alpha:]][[:alpha:]]?\\d\\w?)")
+    stringr::str_extract(postcode, "^([[:alpha:]][[:alpha:]]?\\d\\w?)")
   # Extract the inward code, this will be a number followed by 2 x (letters or numbers)
   inward_code <-
-    stringr::str_extract(postcode_var, "(\\d[[:alpha:]][[:alpha:]])$")
+    stringr::str_extract(postcode, "(\\d[[:alpha:]][[:alpha:]])$")
 
   # Stick them back together padding with the correct number of spaces.
   return(paste0(
