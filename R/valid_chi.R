fc1cdba7fddb12f02b075991d7826cd39bf9b3f8
James McMahon <jamesmcmahon0@gmail.com>
2019-07-17 16:06:53 +0100

    Making changes to speed up valid_chi * Refactored so that if the chi is bad it fails faster * removed reliance on lubridate * removed reliance on stringr Altogether gives a 20X speedup and a 1/5 reduction in memory

--- R/valid_chi.R
+++ R/valid_chi.R
@@ -10,27 +10,41 @@
 valid_chi <- function(chi_number) {
   purrr::map_lgl(chi_number, function(chi_number) {
     # Check the length
-    if (stringr::str_length(chi_number) != 10) {
+    if (nchar(chi_number) != 10) {
       return(FALSE)
     }
 
-    # Look at the check digit by recalculating it and comparing
-    chi_number <- purrr::map_int(stringr::str_split(chi_number, "", simplify = TRUE), as.integer)
-    sum_1_9 <- sum(chi_number[1:9] * (11 - seq_along(chi_number[1:9])))
+    # Split the chi number into a vector
+    chi_number_vec <- unlist(strsplit(chi_number, ""))
+
+    # Check the month is possible
+    month <- as.integer(paste0(chi_number_vec[3], chi_number_vec[4]))
+
+    if (month > 12) {
+      return(FALSE)
+    }
+
+    # Check the day is possible
+    day <- as.integer(paste0(chi_number_vec[1], chi_number_vec[2]))
+
+    # It doesn't deal with leap years (will assume 29 days for Feb always)
+    # day must be <= the number of days for the given month
+    if (day > c(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[month]) {
+      return(FALSE)
+    }
+
+    # Convert chi_number_vec into a vector of integers
+    chi_number_int_vec <- as.integer(chi_number_vec)
+
+    # Calculte the check digit and compare (digit 10)
+    sum_1_9 <- sum(chi_number_int_vec[1:9] * (11 - seq_along(chi_number_int_vec[1:9])))
     check_digit <- ((11 - (sum_1_9 %% 11)) %% 11)
 
-    if (check_digit == chi_number[10]) {
-      # If the check digit matches see if the dates look valid
-      day <- as.integer(paste0(chi_number[1], chi_number[2]))
-      month <- as.integer(paste0(chi_number[3], chi_number[4]))
-
-      # This checks if the days in the month look valid
-      # It doesn't deal with leap years (will assume 29 days for Feb always)
-      # It will also give FALSE for an invalid month
-      if (day <= max(29, lubridate::days_in_month(month), na.rm = TRUE)) {
-        return(TRUE)
-      }
+    if (check_digit != chi_number_int_vec[10]) {
+      return(FALSE)
     }
-    return(FALSE)
+
+    # If the funtion hasn't exited yet, it has passed all the above tests
+    return(TRUE)
   })
 }
